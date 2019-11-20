8931f2740ec3c27d917871e3c025b6a0aa8165a7
Gabriel Song <gabsong4@gmail.com>
2019-11-19 18:32:53 -0800

    Implement the JSON.stringify() method from scratch

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number') {
+    return obj.toString(10);
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      return '[' + obj.map((item, i) => stringifyJSON(item)).join(',') + ']';
+    }
+  } else if (Object(obj) === obj) {
+    let result = [];
+    for (key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        delete obj[key];
+      } else {
+        result.push(`${stringifyJSON(key)}:${stringifyJSON(obj[key])}`);
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
 };
